16445de85a7d45d93266db3b69ac0b5811b8af77
HideSue <HideSue@github.com>
2019-09-24 00:36:52 +0800

    add chapter03

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function about() {
     }
 
     var info = "<div class='info'>"+
-    "<img src='head_background.jpg' alt='' style='width: 100px; height: 100px; border-radius: 50%;'>"+
+    "<img src='index_icon/head_background.jpg' alt='' style='width: 100px; height: 100px; border-radius: 50%;'>"+
     "<p>姓名: 苏梓扬</p>"+
     "<hr>"+
     "<p>学号：2017053579</p>"+
@@ -46,10 +46,20 @@ function hw() {
       $('.other').remove();
     }
     var hw = "<div class='hw'>"+
-    "<img src='first.PNG' alt='' style='width: 100px; height: 130px; display: inline; float: left;'>"+
-    "<div class='word' style='overflow: hidden; margin-right: 0; text-align: center;'>"+
-    "<a target='_blank' href='hm1/project1_1.html'>Project 1</a>"+
-    "<a target='_blank' href='hm1/project1_2.html'>Project 2</a>"+
+    "<img src='index_icon/first.PNG' alt='' style='width: 100px; height: 130px; display: inline; float: left;'>"+
+    "<div class='word' style='margin-right: 0; text-align: center; float: left; padding-left: 10px;'>"+
+    "<a><b>Chapter 2</b></a>"+
+    "<a target='_blank' href='chapter02/project01.html'>Project 1</a>"+
+    "<a target='_blank' href='chapter02/project02.html'>Project 2</a>"+
+    "<time style='font-size: 22px;'>2019-9-21</time>"+
+    "</div>"+
+    "</div>"+
+    "<div class='hw'>"+
+    "<img src='index_icon/first.PNG' alt='' style='width: 100px; height: 130px; display: inline; float: left;'>"+
+    "<div class='word' style='margin-right: 0; text-align: center; float: left; padding-left: 10px;'>"+
+    "<a><b>Chapter 3</b></a>"+
+    "<a target='_blank' href='chapter03/html/project01.html'>Project 1</a>"+
+    "<a target='_blank' href='chapter03/html/project03.html'>Project 3</a>"+
     "<time style='font-size: 22px;'>2019-9-21</time>"+
     "</div>"+
     "</div>";
